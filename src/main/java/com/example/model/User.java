135443af59ebe86b5dcb20937848974a85c0c6c4
Ranjeet-Sahu07 <23BET10386@cuchd.in>
2025-11-05 23:09:25 +0530

    Create User.java

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,77 @@
+package com.example.model;
+
+/**
+ * User model class representing a user in the quiz application
+ * This class stores user information retrieved from users.xml
+ */
+public class User {
+    private String userId;
+    private String username;
+    private String password;
+    private String email;
+    private String role; // "user" or "admin"
+
+    // Default constructor
+    public User() {
+    }
+
+    // Parameterized constructor
+    public User(String userId, String username, String password, String email, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
